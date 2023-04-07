5c97c0ffc45a20c9e72eb8b2213a4f16f43688c3
DaniilKalambetov123123 <84600463+DaniilKalambetov123123@users.noreply.github.com>
2023-04-07 21:29:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import erlang
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 657892082 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+alpha = 1 - p
+    return (2 * x.mean() - 1 + 2 * erlang.ppf(alpha / 2, len(x), loc = 0, scale = 1 / len(x))) / 9025, \
+                      (2 * x.mean() - 1 + 2 * erlang.ppf(1 - alpha / 2, len(x), loc = 0, scale = 1 / len(x))) / 9025
