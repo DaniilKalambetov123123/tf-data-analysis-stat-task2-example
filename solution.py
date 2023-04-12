dbc49bf167c9a5cdc831ba2bd253f3b32c009e3a
DaniilKalambetov123123 <84600463+DaniilKalambetov123123@users.noreply.github.com>
2023-04-12 18:52:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,6 @@ from scipy.stats import erlang
 chat_id = 657892082 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-alpha = 1 - p
+    alpha = 1 - p
     return (2 * x.mean() - 1 + 2 * erlang.ppf(alpha / 2, len(x), loc = 0, scale = 1 / len(x))) / 9025, \
                       (2 * x.mean() - 1 + 2 * erlang.ppf(1 - alpha / 2, len(x), loc = 0, scale = 1 / len(x))) / 9025
